d8a771b5ceaf6298b31220e5e157e9f1849806bc
shuc@kw60939 <oliver.c.shu@gmail.com>
2022-08-03 23:26:02 +0300

    feat: implement reward to go

--- agents/pg_agent.py
+++ agents/pg_agent.py
@@ -10,6 +10,7 @@ class PGAgent(BaseAgent):
         BaseAgent.__init__(self, config)
 
         self.gamma = self.hparams["discount_rate"]
+        self.reward_to_go = self.hparams["reward_to_go"]
         self.actor = GATPolicy(self.hparams)
 
 
@@ -23,28 +24,55 @@ class PGAgent(BaseAgent):
             params:
                 obs: list\n
                 acs: np.ndarray\n
-                rews: np.ndarray\n
+                rews: list\n
                 next_obs: list\n
                 dones: np.ndarray\n
             returns:
                 train_log: dict
         """
-        discounted_return = self._calculate_discounted_return(rews)
-        train_log = self.actor.update(obs, acs, discounted_return)
+        q_values = self.calculate_q_values(rews)
+        train_log = self.actor.update(obs, acs, q_values)
         return train_log
+
+
+    def calculate_q_values(self, rews):
+        """Monte Carlo estimation of the Q function"""
+        if not self.reward_to_go:
+            discounted_returns = [self._discounted_return(r) for r in rews]
+            q_values = np.concatenate(discounted_returns).astype('float32')
+        else:
+            discounted_cumsums = [self._discounted_cumsum(r) for r in rews]
+            q_values = np.concatenate(discounted_cumsums).astype('float32')
+
+        return q_values
 
 
     def save(self, path):
         self.actor.save(path)
 
 
-    def _calculate_discounted_return(self, rewards: np.ndarray) -> np.ndarray:
-        """Calculates the discounted returns for list of trajectories\n
+    def _discounted_return(self, reward):
+        """Calculates the discounted returns for a trajectory\n
             Input:
-                np.ndarray of rewards {r_0, r_1, ..., r_T} for a single trajectory of len T\n
+                np.ndarray reward {r_0, r_1, ..., r_T} for a trajectory of len T\n
             Output:
                 np.ndarray where each index t contains sum_{t'=0}^T gamma^{t'} r_{t'}
         """
-        discounts = self.gamma ** np.arange(rewards.shape[0])
-        discounted_return = np.multiply(discounts, rewards)
+        traj_len = reward.shape[0]
+        discount = self.gamma ** np.arange(traj_len)
+        discounted_reward = np.multiply(discount, reward)
+        discounted_return = np.ones(traj_len) * np.sum(discounted_reward)
         return discounted_return
+
+
+    def _discounted_cumsum(self, reward):
+        """"""
+        traj_len = reward.shape[0]
+        discounted_cumsum = np.zeros(traj_len)
+
+        for t in range(traj_len):
+            discount = self.gamma ** (np.arange(t, traj_len) - t)
+            discounted_reward_to_go = np.multiply(discount, reward[t:])
+            discounted_cumsum[t] = np.sum(discounted_reward_to_go)
+
+        return discounted_cumsum
